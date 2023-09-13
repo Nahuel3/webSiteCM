a13ad71b508e660b1e3fbb8000cfdac723fcbdec
Nahuel <nahuel.valentin@hotmail.com>
2023-09-13 19:51:59 -0300

    arregloBotonPaginaPrincipal

--- src/AppPrincipal.jsx
+++ src/AppPrincipal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './nav';
 import Section from './section';
 import Footer from './footer';
@@ -15,8 +15,9 @@ function App() {
           <Route path="/" element={<Section />} />
           <Route path="/servicios" element={<ServicesPage />} />
           <Route path="/contacto" element={<Form />} />
+          {/* Ruta comodín para redirigir a la página principal */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-     
         <Footer />
       </div>
     </Router>
